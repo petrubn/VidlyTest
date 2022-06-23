1a8b2b528b00b37285527ebe24ca934336b856bc
petrubn <buruian.petru@ceiti.md>
2022-06-23 18:33:49 +0300

    8.6 Done

---
+++ TestAuth2/Migrations/202206231418152_InitMirations.cs
@@ -0,0 +1,74 @@
+﻿namespace TestAuth2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitMirations : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        IsSubscribedToNewsLetter = c.Boolean(nullable: false),
+                        MembershipTypeId = c.Int(),
+                        BirthDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.MembershipTypes", t => t.MembershipTypeId)
+                .Index(t => t.MembershipTypeId);
+            
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Int(nullable: false),
+                        DiscountRate = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        GenreId = c.Int(nullable: false),
+                        AddedDate = c.DateTime(),
+                        StockNumber = c.Int(nullable: false),
+                        ReleaseDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
+                .Index(t => t.GenreId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropTable("dbo.Movies");
+            DropTable("dbo.Genres");
+            DropTable("dbo.MembershipTypes");
+            DropTable("dbo.Customers");
+        }
+    }
+}
